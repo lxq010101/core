579214bd94e0720b0415c108b6b197a3f09d1b09
lxq010101 <781334486@qq.com>
2017-12-18 17:54:47 +0800

    fitst commit

---
+++ app/src/main/java/com/xueqili/myapplication2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xueqili.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.location.AMapLocation;
+import com.xueqili.mylibrary.MyCar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
